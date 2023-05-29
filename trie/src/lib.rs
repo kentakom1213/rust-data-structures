88b8cf807532282df233808b90bbd2b92e7f0d9b
kentakom1213 <kentakom1213@gmail.com>
2023-05-29 19:09:29 +0900

    [add] dictとしての機能を追加

--- trie/src/lib.rs
+++ trie/src/lib.rs
@@ -1,10 +1,6 @@
 /// # trie
 /// - トライ木の実装
 pub mod trie {
-    use std::mem::{
-        replace,
-        swap,
-    };
 
     // 定数
     const ORIGIN: char = 'a'; // 基準となる文字
@@ -12,24 +8,29 @@ pub mod trie {
     const KINDS: usize = 26; // 文字の種類数
     type NodePointer<T> = Option<Box<TrieNode<T>>>;
 
-    /// 何番目の文字かを判定する関数
+    /// 何番目の文字かを判定する
     fn ord(c: char) -> usize {
         let num = c as u32 as usize;
         num - ORIGIN_ID
     }
 
+    /// i番目の文字を返す
+    fn chr(i: usize) -> char {
+        (ORIGIN_ID + i) as u8 as char
+    }
+
     #[derive(Debug, Clone)]
     struct TrieNode<T> {
-        data: T,
+        data: Option<T>,
         children: Vec<NodePointer<T>>,
     }
 
     impl<T> TrieNode<T>
     where
-        T: Default + Clone,
+        T: Clone,
     {
         pub fn new() -> Self {
-            Self { data: T::default(), children: vec![NodePointer::None; KINDS] }
+            Self { data: None, children: vec![NodePointer::None; KINDS] }
         }
     }
 
@@ -40,7 +41,7 @@ pub mod trie {
 
     impl<T> Trie<T>
     where
-        T: Default + Clone,
+        T: Clone,
     {
         // self.originを基準にした文字の番号を返す
         // fn ord()
@@ -48,19 +49,50 @@ pub mod trie {
         pub fn new() -> Self {
             Trie {
                 root: Some(Box::new(TrieNode {
-                    data: T::default(),
+                    data: None,
                     children: vec![NodePointer::None; KINDS],
                 })),
             }
         }
 
-        pub fn insert(&mut self, s: &str) {
+        pub fn insert(&mut self, s: &str, data: T) {
             let mut node = &mut self.root;
+            let mut idx = 0;
             for c in s.chars().map(ord) {
                 if node.as_ref().is_none() {
                     *node = Some(Box::new(TrieNode::new()));
                 }
                 node = node.as_mut().unwrap().children.get_mut(c).unwrap();
+                if idx + 1 == s.len() {
+                    *node = Some(Box::new(
+                        TrieNode { data: Some(data), children: vec![None; KINDS] }
+                    ));
+                    break;
+                }
+                idx += 1;
+            }
+        }
+
+        pub fn traverse(&self) -> Vec<String> {
+            let mut res = vec![];
+            let mut cur = String::new();
+            traverse_inner(&self.root, &mut cur, &mut res);
+            res
+        }
+    }
+
+    /// trieを順に探索する
+    fn traverse_inner<T>(node: &NodePointer<T>, cur: &mut String, list: &mut Vec<String>) {
+        if node.as_ref().unwrap().data.is_some() {
+            list.push(cur.clone());
+        }
+        if let Some(node) = node.as_deref() {
+            for (i, child) in node.children.iter().enumerate() {
+                if child.as_ref().is_some() {
+                    cur.push(chr(i));
+                    traverse_inner(child, cur, list);
+                    cur.pop();
+                }
             }
         }
     }
@@ -77,10 +109,13 @@ mod test {
         let mut trie: Trie<usize> = trie::Trie::new();
 
         // 文字列の挿入
-        trie.insert("powell");
-        trie.insert("powell");
-        trie.insert("powell");
+        trie.insert("powell", 1);
 
+        // デバッグ
         println!("{:#?}", trie);
+
+        // 一覧表示
+        let dict = trie.traverse();
+        println!("{:#?}", dict);
     }
 }
