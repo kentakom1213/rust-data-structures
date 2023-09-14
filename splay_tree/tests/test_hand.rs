72013e713e9a24fda44109b49c21b1a9468ad71c
kentakom1213 <kentakom1213@gmail.com>
2023-09-14 23:21:36 +0900

    [fix] テストコードの集約

--- splay_tree/tests/test_hand.rs
+++ splay_tree/tests/test_hand.rs
@@ -1,7 +1,6 @@
 #![cfg(test)]
 
 use splay_tree::splay_tree::*;
-use splay_tree::tree;
 
 #[test]
 fn test_insert_random() {
@@ -49,90 +48,4 @@ fn test_insert_sorted() {
     // assert_eq!(tree.search(&3), true);
     // assert_eq!(tree.search(&5), true);
     // assert_eq!(tree.search(&1), true);
-}
-
-#[test]
-fn test_splay_left() {
-    let mut tree = SplayTree::new();
-
-    tree.root = tree! {
-        key: 3,
-        value: "3",
-        left: tree! {
-            key: 2,
-            value: "2",
-            left: tree! {
-                key: 1,
-                value: "1",
-                left: tree! {
-                    key: 0,
-                    value: "0",
-                }
-            }
-        }
-    };
-
-    println!("----- before -----");
-    println!("{:?}", &tree);
-
-    tree.splay(&2);
-
-    println!("----- after -----");
-    println!("{:?}", &tree);
-}
-
-#[test]
-fn test_splay_left_left() {
-    let mut tree = SplayTree::new();
-
-    tree.root = tree! {
-        key: 3,
-        value: "3",
-        left: tree! {
-            key: 2,
-            value: "2",
-            left: tree! {
-                key: 1,
-                value: "1",
-                left: tree! {
-                    key: 0,
-                    value: "0",
-                }
-            }
-        }
-    };
-
-    println!("----- before -----");
-    println!("{:?}", &tree);
-
-    tree.splay(&0);
-
-    println!("----- after -----");
-    println!("{:?}", &tree);
-}
-
-#[test]
-fn test_splay_left_right() {
-    let mut tree = SplayTree::new();
-
-    tree.root = tree! {
-        key: 2,
-        value: "2",
-        left: tree! {
-            key: 0,
-            value: "0",
-            right: tree! {
-                key: 1,
-                value: "1",
-            }
-        }
-    };
-
-    println!("----- before -----");
-    println!("{:?}", &tree);
-
-    tree.splay(&1);
-
-    println!("----- after -----");
-    println!("{:?}", &tree);
 }
--- splay_tree/tests/test_splay.rs
+++ splay_tree/tests/test_splay.rs
@@ -3,27 +3,113 @@
 use splay_tree::{splay_tree::*, tree};
 
 #[test]
-fn test_from_path() {
-    let mut tree: SplayTree<u8, i32> = SplayTree::new();
+fn test_splay_left() {
+    let mut tree = SplayTree::new();
 
     tree.root = tree! {
-        key: 0,
-        value: 0,
-        right: tree! {
-            key: 1,
-            value: 1,
+        key: 3,
+        value: "3",
+        left: tree! {
+            key: 2,
+            value: "2",
+            left: tree! {
+                key: 1,
+                value: "1",
+                left: tree! {
+                    key: 0,
+                    value: "0",
+                }
+            }
+        }
+    };
+
+    println!("----- before -----");
+    println!("{:?}", &tree);
+
+    tree.splay(&2);
+
+    println!("----- after -----");
+    println!("{:?}", &tree);
+}
+
+#[test]
+fn test_splay_left_left() {
+    let mut tree = SplayTree::new();
+
+    tree.root = tree! {
+        key: 3,
+        value: "3",
+        left: tree! {
+            key: 2,
+            value: "2",
+            left: tree! {
+                key: 1,
+                value: "1",
+                left: tree! {
+                    key: 0,
+                    value: "0",
+                }
+            }
+        }
+    };
+
+    println!("----- before -----");
+    println!("{:?}", &tree);
+
+    tree.splay(&0);
+
+    println!("----- after -----");
+    println!("{:?}", &tree);
+}
+
+#[test]
+fn test_splay_left_right() {
+    let mut tree = SplayTree::new();
+
+    tree.root = tree! {
+        key: 2,
+        value: "2",
+        left: tree! {
+            key: 0,
+            value: "0",
             right: tree! {
-                key: 2,
-                value: 2,
-                right: tree! {
-                    key: 3,
-                    value: 3,
-                    right: tree! {
-                        key: 4,
-                        value: 4,
-                        right: tree! {
-                            key: 5,
-                            value: 5,
+                key: 1,
+                value: "1",
+            }
+        }
+    };
+
+    println!("----- before -----");
+    println!("{:?}", &tree);
+
+    tree.splay(&1);
+
+    println!("----- after -----");
+    println!("{:?}", &tree);
+}
+
+#[test]
+fn test_from_path() {
+    let mut tree: SplayTree<u8, &str> = SplayTree::new();
+
+    tree.root = tree! {
+        key: 5,
+        value: "alpha",
+        left: tree! {
+            key: 4,
+            value: "beta",
+            left: tree! {
+                key: 3,
+                value: "gamma",
+                left: tree! {
+                    key: 2,
+                    value: "delta",
+                    left: tree! {
+                        key: 1,
+                        value: "epsilon",
+                        left: tree! {
+                            key: 0,
+                            value: "zeta",
                         }
                     }
                 }
@@ -33,9 +119,6 @@ fn test_from_path() {
 
     println!("### before splay ###");
     println!("{:?}", &tree);
-
-    assert_eq!(tree.search(&5), Some(&5));
-    assert_eq!(tree.search(&6), None);
 
     // スプレー操作を行う
     tree.splay(&0);
