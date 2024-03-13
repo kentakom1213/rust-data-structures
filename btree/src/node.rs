5b61c6693973cfededcc94c2636914a7b4e68770
kentakom1213 <kentakom1213@gmail.com>
2024-03-14 02:02:17 +0900

    [add] DEGに統一

--- btree/src/node.rs
+++ btree/src/node.rs
@@ -6,25 +6,31 @@ use std::{
 };
 
 /// B木のノード
-pub enum Node<K, V, const CAP: usize, const DEG: usize> {
+/// - `DEG`：ノードの持つデータの数
+/// - `DEG`：子ノードの数
+///
+pub enum Node<K, V, const DEG: usize>
+where
+    [(); DEG - 1]:,
+{
     /// 葉ノード
     Leaf {
         /// 親へのポインタ
-        parent: Option<Weak<RefCell<Node<K, V, CAP, DEG>>>>,
+        parent: Option<Weak<RefCell<Node<K, V, DEG>>>>,
         /// キーの配列
-        keys: [K; CAP],
+        keys: [K; DEG - 1],
         /// 値の配列
-        vals: [V; CAP],
+        vals: [V; DEG - 1],
     },
     /// 内部ノード
     Internal {
         /// 親へのポインタ
-        parent: Option<Weak<RefCell<Node<K, V, CAP, DEG>>>>,
+        parent: Option<Weak<RefCell<Node<K, V, DEG>>>>,
         /// キーの配列
-        keys: [K; CAP],
+        keys: [K; DEG - 1],
         /// 値の配列
-        vals: [V; CAP],
+        vals: [V; DEG - 1],
         /// 子
-        children: [Option<Rc<RefCell<Node<K, V, CAP, DEG>>>>; DEG],
+        children: [Option<Rc<RefCell<Node<K, V, DEG>>>>; DEG],
     },
 }
