3db08d0d2e19b05ce9dd7fa58fd2d2d7b96e6cf1
kentakom1213 <kentakom1213@gmail.com>
2024-08-17 23:28:02 +0900

    [add] splayの実装

--- splay_tree/src/node.rs
+++ splay_tree/src/node.rs
@@ -8,6 +8,8 @@ use std::{
     rc::{Rc, Weak},
 };
 
+use crate::print_util::print_as_binary_tree;
+
 /// ノードの構造体
 pub struct Node<K: Ord, V> {
     pub key: K,
@@ -114,13 +116,116 @@ pub fn find<K: Ord, V>(root: NodePtr<K, V>, x: &K) -> (NodePtr<K, V>, NodePtr<K,
 
 /// nodeを1つ上に持ってくるように回転する
 fn rotate<K: Ord, V>(node: NodePtr<K, V>) -> NodePtr<K, V> {
-    let mut node = node?;
-    // 親
-    let Some(par) = node.as_ref().borrow_mut().parent.take() else {
-        return Some(node);
-    };
+    match NodeState::get(&node) {
+        NodeState::Nil | NodeState::Root => node,
+        NodeState::LeftChild => {
+            let inner = node?;
+            let mut right = inner.as_ref().borrow_mut().right.take();
+            let par = inner.as_ref().borrow().parent.clone()?;
 
-    todo!()
+            // 親の左の子←自分の右の子
+            if let Some(right) = &mut right {
+                right.borrow_mut().parent = Some(par.clone());
+            }
+            let par = par.upgrade()?;
+            par.borrow_mut().left = right;
+
+            // 自分の親←親の親
+            let par_state = NodeState::get(&Some(par.clone()));
+            let mut parpar = par.borrow_mut().parent.take();
+            if let Some(parpar) = &mut parpar {
+                match par_state {
+                    NodeState::LeftChild => {
+                        parpar.upgrade()?.borrow_mut().left = Some(inner.clone());
+                    }
+                    NodeState::RightChild => {
+                        parpar.upgrade()?.borrow_mut().right = Some(inner.clone());
+                    }
+                    _ => (),
+                }
+            }
+            inner.borrow_mut().parent = parpar;
+
+            // 自分の右の子←親
+            par.borrow_mut().parent = Some(Rc::downgrade(&inner));
+            inner.borrow_mut().right = Some(par);
+
+            Some(inner)
+        }
+        NodeState::RightChild => {
+            let inner = node?;
+            let mut left = inner.as_ref().borrow_mut().left.take();
+            let par = inner.as_ref().borrow().parent.clone()?;
+
+            // 親の右の子←自分の左の子
+            if let Some(left) = &mut left {
+                left.borrow_mut().parent = Some(par.clone());
+            }
+            let par = par.upgrade()?;
+            par.borrow_mut().right = left;
+
+            // 自分の親←親の親
+            let par_state = NodeState::get(&Some(par.clone()));
+            let mut parpar = par.borrow_mut().parent.take();
+            if let Some(parpar) = &mut parpar {
+                match par_state {
+                    NodeState::LeftChild => {
+                        parpar.upgrade()?.borrow_mut().left = Some(inner.clone());
+                    }
+                    NodeState::RightChild => {
+                        parpar.upgrade()?.borrow_mut().right = Some(inner.clone());
+                    }
+                    _ => (),
+                }
+            }
+            inner.borrow_mut().parent = parpar;
+
+            // 自分の左の子←親
+            par.borrow_mut().parent = Some(Rc::downgrade(&inner));
+            inner.borrow_mut().left = Some(par);
+
+            Some(inner)
+        }
+    }
+}
+
+/// スプレー操作によりnodeを根に移動し，新たな根を返す
+fn splay<K: Ord, V>(mut node: NodePtr<K, V>) -> NodePtr<K, V> {
+    let mut state = NodeState::get(&node);
+    while state.is_child() {
+        // 親頂点の状態
+        let par_state = NodeState::get_from_weak(&node.as_ref()?.borrow().parent);
+
+        match (state, par_state) {
+            // zig
+            (NodeState::LeftChild | NodeState::RightChild, NodeState::Root) => {
+                node = rotate(node);
+            }
+            // zig-zig
+            (NodeState::LeftChild, NodeState::RightChild)
+            | (NodeState::RightChild, NodeState::LeftChild) => {
+                node = rotate(node);
+                node = rotate(node);
+            }
+            // zig-zag
+            (NodeState::LeftChild, NodeState::LeftChild)
+            | (NodeState::RightChild, NodeState::RightChild) => {
+                // 親を先にrotate（オブジェクトをdropさせないため，変数に代入する）
+                let _par = rotate(
+                    node.clone()?
+                        .borrow()
+                        .parent
+                        .as_ref()
+                        .map(|p| p.upgrade().unwrap()),
+                );
+                node = rotate(node);
+            }
+            _ => unreachable!(),
+        }
+
+        state = NodeState::get(&node);
+    }
+    node
 }
 
 /// ノードの状態を調べる
@@ -153,11 +258,26 @@ impl NodeState {
         };
         // 左の子である場合
         let par = par.upgrade().unwrap();
-        if Rc::ptr_eq(par.borrow().left.as_ref().unwrap(), &inner) {
+        if par
+            .borrow()
+            .left
+            .as_ref()
+            .is_some_and(|left| Rc::ptr_eq(left, inner))
+        {
             NodeState::LeftChild
         } else {
             NodeState::RightChild
         }
+    }
+
+    fn get_from_weak<K: Ord, V>(node: &ParentPtr<K, V>) -> Self {
+        let node = node.as_ref().map(|p| p.upgrade().unwrap());
+        Self::get(&node)
+    }
+
+    /// 子頂点であるかを判定する
+    fn is_child(&self) -> bool {
+        matches!(self, Self::LeftChild | Self::RightChild)
     }
 }
 
@@ -195,7 +315,7 @@ impl<K: Ord + Debug, V: Debug> Debug for Node<K, V> {
 #[cfg(test)]
 mod test {
     use crate::{
-        node::{insert, NodeState},
+        node::{insert, rotate, splay, NodeState},
         print_util::print_as_binary_tree,
     };
 
@@ -272,17 +392,29 @@ mod test {
 
         print_as_binary_tree(&root);
 
+        let find_1;
+        (root, find_1) = find(root, &1);
+        println!("find_1 = {:?}", NodeState::get(&find_1));
+
+        let find_3;
+        (root, find_3) = find(root, &3);
+        println!("find_3 = {:?}", NodeState::get(&find_3));
+
         let find_5;
         (root, find_5) = find(root, &5);
         println!("find_5 = {:?}", NodeState::get(&find_5));
+
+        let find_15;
+        (root, find_15) = find(root, &15);
+        println!("find_15 = {:?}", NodeState::get(&find_15));
 
         let find_20;
         (root, find_20) = find(root, &20);
         println!("find_20 = {:?}", NodeState::get(&find_20));
 
-        let find_15;
-        (root, find_15) = find(root, &15);
-        println!("find_15 = {:?}", NodeState::get(&find_15));
+        let find_30;
+        (root, find_30) = find(root, &30);
+        println!("find_30 = {:?}", NodeState::get(&find_30));
 
         (root, _) = insert(root, 20, "sixth");
         print_as_binary_tree(&root);
@@ -290,5 +422,140 @@ mod test {
         let find_20;
         (root, find_20) = find(root, &20);
         println!("find_20 = {:?}", NodeState::get(&find_20));
+    }
+
+    #[test]
+    fn test_rotate_right() {
+        let mut root = None;
+        (root, _) = insert(root, 5, "first");
+        (root, _) = insert(root, 15, "second");
+        (root, _) = insert(root, 1, "third");
+        (root, _) = insert(root, 3, "forth");
+        (root, _) = insert(root, 30, "fifth");
+
+        print_as_binary_tree(&root);
+
+        let find_5;
+        (root, find_5) = find(root, &5);
+        println!("find_5 = {:?}", NodeState::get(&find_5));
+
+        // rootを回転
+        println!("> rotate at root");
+        root = rotate(root);
+
+        print_as_binary_tree(&root);
+
+        {
+            let mut find_1;
+            (root, find_1) = find(root, &1);
+            println!("find_1 = {:?}", NodeState::get(&find_1));
+
+            // 回転
+            println!("> rotate 1");
+            find_1 = rotate(find_1);
+
+            print_as_binary_tree(&root);
+            print_as_binary_tree(&find_1);
+
+            println!("root = {:?}", NodeState::get(&root));
+            println!("find_1 = {:?}", NodeState::get(&find_1));
+
+            root = find_1;
+        }
+
+        {
+            let mut find_3;
+            (root, find_3) = find(root, &3);
+            println!("find_3 = {:?}", NodeState::get(&find_3));
+
+            // 30を回転
+            println!("> rotate 3");
+            find_3 = rotate(find_3);
+
+            print_as_binary_tree(&root);
+
+            println!("root = {:?}", NodeState::get(&root));
+            println!("find_3 = {:?}", NodeState::get(&find_3));
+        }
+    }
+
+    #[test]
+    fn test_rotate_left() {
+        let mut root = None;
+        (root, _) = insert(root, 5, "first");
+        (root, _) = insert(root, 15, "second");
+        (root, _) = insert(root, 1, "third");
+        (root, _) = insert(root, 3, "forth");
+        (root, _) = insert(root, 30, "fifth");
+
+        print_as_binary_tree(&root);
+
+        {
+            let mut find_30;
+            (root, find_30) = find(root, &30);
+            println!("find_30 = {:?}", NodeState::get(&find_30));
+
+            // 回転
+            println!("> rotate 30");
+            find_30 = rotate(find_30);
+
+            print_as_binary_tree(&root);
+            print_as_binary_tree(&find_30);
+
+            println!("root = {:?}", NodeState::get(&root));
+            println!("find_30 = {:?}", NodeState::get(&find_30));
+        }
+
+        {
+            let mut find_30;
+            (root, find_30) = find(root, &30);
+            println!("find_30 = {:?}", NodeState::get(&find_30));
+
+            // 回転
+            println!("> rotate 30");
+            find_30 = rotate(find_30);
+
+            print_as_binary_tree(&root);
+            print_as_binary_tree(&find_30);
+
+            println!("root = {:?}", NodeState::get(&root));
+            println!("find_30 = {:?}", NodeState::get(&find_30));
+
+            root = find_30;
+        }
+    }
+
+    #[test]
+    fn test_splay() {
+        let mut root = None;
+
+        (root, _) = insert(root, 1, "first");
+        (root, _) = insert(root, 3, "second");
+        (root, _) = insert(root, 4, "third");
+        (root, _) = insert(root, 9, "forth");
+        (root, _) = insert(root, 2, "fifth");
+
+        print_as_binary_tree(&root);
+
+        let node;
+        (root, node) = find(root, &4);
+
+        root = splay(node);
+
+        print_as_binary_tree(&root);
+
+        let node;
+        (root, node) = find(root, &1);
+
+        root = splay(node);
+
+        print_as_binary_tree(&root);
+
+        let node;
+        (root, node) = find(root, &9);
+
+        root = splay(node);
+
+        print_as_binary_tree(&root);
     }
 }
