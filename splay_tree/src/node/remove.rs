fb2cf5f822c3e69e7649e8e5a4768784964b120c
kentakom1213 <kentakom1213@gmail.com>
2024-08-30 10:30:14 +0900

    [add] remove

--- splay_tree/src/node/remove.rs
+++ splay_tree/src/node/remove.rs
@@ -13,15 +13,37 @@ use super::{
 ///
 /// **戻り値**
 /// - NodePtr\<K, V\>: 削除後の木の根のポインタ
-/// - Option\<(K, V)\>: 削除されたノードのキー，値
+/// - NodePtr\<K, V\>: 削除されたノードのポインタ
 pub fn remove<K: Ord, V>(
-    root: NodePtr<K, V>,
-    node: NodePtr<K, V>,
-) -> (NodePtr<K, V>, Option<(K, V)>) {
-    todo!()
+    mut root: NodePtr<K, V>,
+    mut node: NodePtr<K, V>,
+) -> (NodePtr<K, V>, NodePtr<K, V>) {
+    let state = node.get_state();
+    let left = node.take_left();
+    let right = node.take_right();
+    let parent = node.take_parent_strong();
+
+    // 葉になったので自分を削除
+    let node = remove_leaf(node);
+
+    match (left.is_some(), right.is_some()) {
+        (false, false) => match state {
+            NodeState::Root => root = None,
+            _ => (),
+        },
+        (false, true) => match state {
+            NodeState::Root => (root, node) = (right, node),
+        },
+        (true, false) => todo!(),
+        (true, true) => todo!(),
+    }
+
+    (root, node)
 }
 
 /// 葉ノード leaf を削除し，削除済み頂点のポインタを返す
+///
+/// （葉ノードであるかの判定は行わない）
 fn remove_leaf<K: Ord, V>(mut leaf: NodePtr<K, V>) -> NodePtr<K, V> {
     // 親ノードから切り離す
     let mut node = match leaf.get_state() {
@@ -40,18 +62,32 @@ fn remove_leaf<K: Ord, V>(mut leaf: NodePtr<K, V>) -> NodePtr<K, V> {
 #[cfg(test)]
 mod test_remove {
     use crate::{
-        node::insert::{self, insert_single},
+        node::{find::find, insert::insert_single, pointer::NodeOps},
         print_util::print_as_binary_tree,
     };
+
+    use super::remove_leaf;
 
     #[test]
     fn test_remove_leaf() {
         let mut root = None;
 
-        for i in 0..10 {
+        for i in [4, 2, 6, 1, 3, 5, 6, 7] {
             (root, _, _) = insert_single(root, i, i);
         }
 
         print_as_binary_tree(&root);
+
+        for i in [1, 3, 5, 7] {
+            let node = find(root.clone(), &i);
+
+            println!("{:?}", node.get_state());
+
+            let node = remove_leaf(node);
+
+            println!("{:?}", node.get_state());
+
+            print_as_binary_tree(&root);
+        }
     }
 }
