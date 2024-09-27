47faa869963712fc357bca0f06cd0defb15b983b
kentakom1213 <kentakom1213@gmail.com>
2024-09-28 01:07:25 +0900

    [fix] stateの修正

--- splay_tree/src/node/state.rs
+++ splay_tree/src/node/state.rs
@@ -16,43 +16,32 @@ pub enum NodeState {
 #[cfg(test)]
 mod test_node_state {
     use crate::{
-        node::{find::find, insert::insert, pointer::NodeOps, state::NodeState},
+        node::{insert::insert, pointer::NodeOps, state::NodeState},
         print_util::print_as_tree,
     };
 
     #[test]
     fn test_nodestate() {
+        let (find_1, find_3, find_5, find_15, find_30);
+        let mut find_20 = None;
+
         let mut root = None;
-        (root, _, _) = insert(root, 5, "first");
-        (root, _, _) = insert(root, 15, "second");
-        (root, _, _) = insert(root, 1, "third");
-        (root, _, _) = insert(root, 3, "forth");
-        (root, _, _) = insert(root, 30, "fifth");
+        (root, find_5, _) = insert(root, 5, "first");
+        (root, find_15, _) = insert(root, 15, "second");
+        (root, find_1, _) = insert(root, 1, "third");
+        (root, find_3, _) = insert(root, 3, "forth");
+        (root, find_30, _) = insert(root, 30, "fifth");
 
         print_as_tree(&root);
-
-        let find_1 = find(root.clone(), &1);
         assert_eq!(find_1.get_state(), NodeState::LeftChild);
-
-        let find_3 = find(root.clone(), &3);
         assert_eq!(find_3.get_state(), NodeState::RightChild);
-
-        let find_5 = find(root.clone(), &5);
         assert_eq!(find_5.get_state(), NodeState::Root);
-
-        let find_15 = find(root.clone(), &15);
         assert_eq!(find_15.get_state(), NodeState::RightChild);
-
-        let find_20 = find(root.clone(), &20);
         assert_eq!(find_20.get_state(), NodeState::Nil);
-
-        let find_30 = find(root.clone(), &30);
         assert_eq!(find_30.get_state(), NodeState::RightChild);
 
-        (root, _, _) = insert(root, 20, "sixth");
+        (root, find_20, _) = insert(root, 20, "sixth");
         print_as_tree(&root);
-
-        let find_20 = find(root.clone(), &20);
         assert_eq!(find_20.get_state(), NodeState::LeftChild);
     }
 }
