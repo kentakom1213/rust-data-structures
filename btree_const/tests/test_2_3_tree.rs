5016e257c682ee548773c59a77db0dce7451230d
kentakom1213 <kentakom1213@gmail.com>
2024-11-22 00:56:37 +0900

    [add] テストファイルの修正

--- btree_const/tests/test_2_3_tree.rs
+++
@@ -1,8 +0,0 @@
-//! 2-3木のテスト
-//! `DEGREE = 3` 以外の場合にはエラー
-
-use btree_const::{btree, debug_util::print_as_tree, node::Node};
-use std::{
-    cell::RefCell,
-    rc::{Rc, Weak},
-};
---
+++ btree_const/tests/test_insert.rs
@@ -0,0 +1,38 @@
+use btree_const::{
+    debug_util::print_as_tree,
+    insert::insert,
+    node::{Node, NodePtr},
+};
+
+/// 空きのあるノードに挿入
+#[test]
+fn test_insert_with_vacent() {
+    let mut tree: Option<NodePtr<u8, &str>> = Some(Node::alloc_leaf());
+
+    // 空きがある
+    assert_eq!(tree.as_ref().unwrap().borrow().has_vacant(), true);
+
+    eprintln!("\n> insert 20");
+    tree = insert(tree, 20, "0020");
+    eprintln!("{:?}", &tree);
+    print_as_tree(&tree);
+
+    // 空きがある
+    assert_eq!(tree.as_ref().unwrap().borrow().has_vacant(), true);
+
+    eprintln!("\n> insert 50");
+    tree = insert(tree, 50, "0050");
+    eprintln!("{:?}", &tree);
+    print_as_tree(&tree);
+
+    // 空きがある
+    assert_eq!(tree.as_ref().unwrap().borrow().has_vacant(), true);
+
+    eprintln!("\n> insert 10");
+    tree = insert(tree, 10, "0010");
+    eprintln!("{:?}", &tree);
+    print_as_tree(&tree);
+
+    // → 満杯になる
+    assert_eq!(tree.as_ref().unwrap().borrow().has_vacant(), false);
+}
